fc33ab4467c6ae2a296f1be892309d365890da2d
Soo <soojlee0106@naver.com>
2025-01-25 18:40:12 +0900

    feat: 기록 클래스 추가

---
+++ lib/models/poling-record.model.dart
@@ -0,0 +1,46 @@
+class PolingRecord {
+  final String id;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final DateTime? deletedAt;
+  final DateTime videoDate;
+  final List<String> tags;
+  final String memo;
+
+  PolingRecord({
+    required this.id,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    this.deletedAt,
+    required this.videoDate,
+    required this.tags,
+    required this.memo,
+  })  : createdAt = createdAt ?? DateTime.now(),
+        updatedAt = updatedAt ?? DateTime.now();
+
+  factory PolingRecord.fromJson(Map<String, dynamic> json) {
+    return PolingRecord(
+      id: json['id'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+      updatedAt: DateTime.parse(json['updatedAt'] as String),
+      deletedAt: json['deletedAt'] != null
+          ? DateTime.parse(json['deletedAt'] as String)
+          : null,
+      videoDate: DateTime.parse(json['videoDate'] as String),
+      tags: List<String>.from(json['tags'] as List),
+      memo: json['memo'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+      'deletedAt': deletedAt?.toIso8601String(),
+      'videoDate': videoDate.toIso8601String(),
+      'tags': tags,
+      'memo': memo,
+    };
+  }
+}
